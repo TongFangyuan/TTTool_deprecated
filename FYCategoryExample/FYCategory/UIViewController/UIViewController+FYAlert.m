371abdb9ea5cb9128d49a7d74269e6800d587785
tongfy <573682532@qq.com>
2018-05-30 17:33:10 +0800

    fix bug on FYAlert

--- FYCategoryExample/FYCategory/UIViewController/UIViewController+FYAlert.m
+++ FYCategoryExample/FYCategory/UIViewController/UIViewController+FYAlert.m
@@ -10,78 +10,74 @@
 
 @implementation UIViewController (FYAlert)
 
-- (void)fy_showTitle:(NSString *)title message:(NSString *)message
-{
+- (void)fy_showTitle:(NSString *)title
+             message:(NSString *)message {
     [self fy_alertActionWithTitle:title message:message style:UIAlertActionStyleDefault handler:nil];
 }
 
-- (void)fy_showTitle:(NSString *)title message:(NSString *)message confirmHandler:(void(^)(void))confirmHandler{
+- (void)fy_showTitle:(NSString *)title
+             message:(NSString *)message
+      confirmHandler:(void(^)(void))confirmHandler {
     
     UIAlertAction *confirmAlertAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        confirmHandler();
+        if (confirmHandler) confirmHandler();
     }];
     
     [self fy_showTitle:title
-                     message:message
-           cancelAlertAction:nil
-          confirmAlertAction:confirmAlertAction];
+               message:message
+     cancelAlertAction:nil
+    confirmAlertAction:confirmAlertAction];
 }
 
 - (void)fy_showTitle:(NSString *)title
-                   message:(NSString *)message
-            confirmHandler:(void(^)(void))confirmHandler
-             cancelHandler:(void(^)(void))cancelHandler{
+             message:(NSString *)message
+      confirmHandler:(void(^)(void))confirmHandler
+       cancelHandler:(void(^)(void))cancelHandler {
     UIAlertAction *confirmAlertAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        confirmHandler();
+        if (confirmHandler) confirmHandler();
     }];
     
     UIAlertAction *cancelAlertAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        cancelHandler();
+        if (cancelHandler) cancelHandler();
     }];
     
     [self fy_showTitle:title
-                     message:message
-           cancelAlertAction:cancelAlertAction
-          confirmAlertAction:confirmAlertAction];
+               message:message
+     cancelAlertAction:cancelAlertAction
+    confirmAlertAction:confirmAlertAction];
 }
 
 
 #pragma mark - private
 
-- (void)fy_alertActionWithTitle:(NSString *)title message:(NSString *)message style:(UIAlertActionStyle)style handler:(void (^)(UIAlertAction *action))handler
+- (void)fy_alertActionWithTitle:(NSString *)title
+                        message:(NSString *)message
+                          style:(UIAlertActionStyle)style
+                        handler:(void (^)(UIAlertAction *action))handler
 {
     
     UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"确定" style:style handler:handler];
     [self fy_showTitle:title
-                     message:message
-           cancelAlertAction:nil
-          confirmAlertAction:confirmAction];
+               message:message
+     cancelAlertAction:nil
+    confirmAlertAction:confirmAction];
     
 }
 
 
 - (void)fy_showTitle:(NSString *)title
-                   message:(NSString *)message
-        confirmAlertAction:(UIAlertAction *)confirmAlertAction{
+             message:(NSString *)message
+  confirmAlertAction:(UIAlertAction *)confirmAlertAction{
     [self fy_showTitle:title
-                     message:message
-           cancelAlertAction:nil
-          confirmAlertAction:confirmAlertAction];
+               message:message
+     cancelAlertAction:nil
+    confirmAlertAction:confirmAlertAction];
 }
 
-
-/**
- *  弹出框控制器
- *
- *  @param title              标题
- *  @param message            提示信息
- *  @param cancelAlertAction  取消事件
- *  @param confirmAlertAction 确定事件
- */
 - (void)fy_showTitle:(NSString *)title
-                   message:(NSString *)message
-         cancelAlertAction:(UIAlertAction *)cancelAlertAction
-        confirmAlertAction:(UIAlertAction *)confirmAlertAction{
+             message:(NSString *)message
+   cancelAlertAction:(UIAlertAction *)cancelAlertAction
+  confirmAlertAction:(UIAlertAction *)confirmAlertAction{
     
     UIAlertController* alert = [UIAlertController
                                 alertControllerWithTitle:title
@@ -100,3 +96,4 @@
 }
 
 @end
+
