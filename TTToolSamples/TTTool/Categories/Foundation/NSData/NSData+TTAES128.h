9a5208e92f0d14cbca0aaa20443fea58308bf107
tongfy <583682532@qq.com>
2019-07-18 14:24:30 +0800

    AES128加密功能

--- TTToolSamples/TTTool/Categories/Foundation/NSData/NSData+TTAES128.h
+++ TTToolSamples/TTTool/Categories/Foundation/NSData/NSData+TTAES128.h
@@ -5,25 +5,72 @@
 //  Created by Tong on 2019/7/18.
 //  Copyright © 2019 tongfy. All rights reserved.
 //
+//  AES128 加密
+//
+//  参考资料
+//  https://www.jianshu.com/p/93466b31f675
+//  https://github.com/yuanshuainiuniu/AES128ECB/blob/master/FBEncryptorAES.h
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-/**
- AES128 加密
- */
 @interface NSData (TTAES128)
 
+/**
+ AES128 ECB 加密
+
+ @param data 需要加密的数据
+ @param key 密钥
+ @return 加密后的数据
+ */
++ (NSData *)tt_AES128ECBEncryptData:(NSData *)data key:(NSData *)key;
 
 /**
- AES128 ECB加密模式
+ AES128 ECB 解密
 
- @param plainText 源数据
- @param key       密钥
- @return          加密后的数据
+ @param data 需要解密的数据
+ @param key 密钥
+ @return 解密后的数据
  */
-+ (NSData *)tt_AES128ECBEncrypt:(NSString *)plainText key:(NSString *)key;
++ (NSData *)tt_AES128ECBDecryptData:(NSData *)data key:(NSData *)key;
+
+/**
+ AES128 PKCS7 加密
+ 
+ @param data 需要加密的数据
+ @param key 密钥
+ @return 加密后的数据
+ */
++ (NSData *)tt_AES128PKCS7EncryptData:(NSData *)data key:(NSData *)key;
+
+/**
+ AES128 PKCS7 解密
+ 
+ @param data 需要解密的数据
+ @param key 密钥
+ @return 解密后的数据
+ */
++ (NSData *)tt_AES128PKCS7DecryptData:(NSData *)data key:(NSData *)key;
+
+/**
+ AES128  ECB、PKCS7两种兼容模式加密
+
+ @param data 需要加密的数据
+ @param key 密钥
+ @return 加密后的数据
+ */
++ (NSData *)tt_AES128EncryptData:(NSData *)data key:(NSData *)key;
+
+/**
+ AES128  ECB、PKCS7两种兼容模式解密
+
+ @param data 需要解密的数据
+ @param key 密钥
+ @return 解密后的数据
+ */
++ (NSData *)tt_AES128DecryptData:(NSData *)data key:(NSData *)key;
+
 
 @end
 
--- TTToolSamples/TTTool/Categories/Foundation/NSData/NSData+TTAES128.m
+++ TTToolSamples/TTTool/Categories/Foundation/NSData/NSData+TTAES128.m
@@ -11,34 +11,113 @@
 
 @implementation NSData (TTAES128)
 
-+ (NSData *)tt_AES128ECBEncrypt:(NSString *)plainText key:(NSString *)key {
-    char keyPtr[kCCKeySizeAES128+1];
-    memset(keyPtr, 0, sizeof(keyPtr));
-    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
++ (NSData *)tt_AES128ECBEncryptData:(NSData *)data key:(NSData *)key {
+    return [self _aes128EncryptData:data key:key options:kCCOptionECBMode];
+}
+
+
++ (NSData *)tt_AES128ECBDecryptData:(NSData *)data key:(NSData *)key {
+    return [self _aes128DecryptData:data key:key options:kCCOptionECBMode];
+}
+
++ (NSData *)tt_AES128PKCS7EncryptData:(NSData *)data key:(NSData *)key {
+    return [self _aes128EncryptData:data key:key options:kCCOptionPKCS7Padding];
+}
+
++ (NSData *)tt_AES128PKCS7DecryptData:(NSData *)data key:(NSData *)key {
+    return [self _aes128DecryptData:data key:key options:kCCOptionPKCS7Padding];
+}
+
++ (NSData *)tt_AES128EncryptData:(NSData *)data key:(NSData *)key {
+    return [self _aes128EncryptData:data key:key options:kCCOptionPKCS7Padding|kCCOptionECBMode];
+}
+
++ (NSData *)tt_AES128DecryptData:(NSData *)data key:(NSData *)key {
+    return [self _aes128DecryptData:data key:key options:kCCOptionPKCS7Padding|kCCOptionECBMode];
+}
+
++ (NSData *)_aes128EncryptData:(NSData *)data
+                           key:(NSData *)key
+                       options:(CCOptions)options
+{
     
-    NSData* data = [plainText dataUsingEncoding:NSUTF8StringEncoding];
-    NSUInteger dataLength = [data length];
+    NSData* result = nil;
     
-    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    // setup key
+    // 设置密钥
+    unsigned char cKey[kCCKeySizeAES128];
+    bzero(cKey, sizeof(cKey));
+    [key getBytes:cKey length:kCCKeySizeAES128];
+    
+    // setup output buffer
+    // 设置输出数据长度
+    size_t bufferSize = [data length] + kCCKeySizeAES128;
     void *buffer = malloc(bufferSize);
-    size_t numBytesEncrypted = 0;
+    
+    // do encrypt
+    // 开始加密
+    size_t encryptedSize = 0;
     CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt,
                                           kCCAlgorithmAES128,
-                                          kCCOptionECBMode,
-                                          keyPtr,
-                                          kCCBlockSizeAES128,
-                                          NULL,
+                                          options,
+                                          cKey,
+                                          kCCKeySizeAES128,
+                                          nil,
                                           [data bytes],
-                                          dataLength,
+                                          [data length],
                                           buffer,
                                           bufferSize,
-                                          &numBytesEncrypted);
+                                          &encryptedSize);
     if (cryptStatus == kCCSuccess) {
-        NSData *resultData = [NSData dataWithBytesNoCopy:buffer length:numBytesEncrypted];
-        return resultData;
+        result = [NSData dataWithBytesNoCopy:buffer length:encryptedSize];
+    } else {
+        free(buffer);
+        NSLog(@"[ERROR] failed to encrypt|CCCryptoStatus: %d", cryptStatus);
     }
-    free(buffer);
-    return nil;
+    
+    return result;
+}
+
++ (NSData *)_aes128DecryptData:(NSData *)data
+                           key:(NSData *)key
+                       options:(CCOptions)options
+{
+    NSData* result = nil;
+    
+    // setup key
+    // 设置密钥
+    unsigned char cKey[kCCKeySizeAES128];
+    bzero(cKey, sizeof(cKey));
+    [key getBytes:cKey length:kCCKeySizeAES128];
+    
+    // setup output buffer
+    // 设置输出数据长度
+    size_t bufferSize = [data length] + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    
+    // do decrypt
+    // 开始解密
+    size_t decryptedSize = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
+                                          kCCAlgorithmAES128,
+                                          options,
+                                          cKey,
+                                          kCCKeySizeAES128,
+                                          nil,
+                                          [data bytes],
+                                          [data length],
+                                          buffer,
+                                          bufferSize,
+                                          &decryptedSize);
+    
+    if (cryptStatus == kCCSuccess) {
+        result = [NSData dataWithBytesNoCopy:buffer length:decryptedSize];
+    } else {
+        free(buffer);
+        NSLog(@"[ERROR] failed to decrypt| CCCryptoStatus: %d", cryptStatus);
+    }
+    
+    return result;
 }
 
 @end
