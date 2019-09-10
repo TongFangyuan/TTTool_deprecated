577f1780c5f407c45391a2f2f7aa70f5ad9298e0
tongfy <583682532@qq.com>
2019-09-10 15:45:37 +0800

    代码检测

--- TTToolSamples/TTTool/Categories/UIKit/UITableView/UITableView+TTEditStyle.m
+++ TTToolSamples/TTTool/Categories/UIKit/UITableView/UITableView+TTEditStyle.m
@@ -11,10 +11,14 @@
 
 - (void)insertSubview:(UIView *)view atIndex:(NSInteger)index{
     [super insertSubview:view atIndex:index];
+    /// ios 11以上是撇
+    if (@available(iOS 11.0, *)) {
     if ([view isKindOfClass:[UITableViewCell class]]) {
         NSLog(@"⚠️ 如果你的tableview出现问题，注视这段代码，再试试🐸");
         [view setNeedsLayout];
     }
+    }
+    
 }
 
 @end
